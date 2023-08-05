3be6b38766bf3cfe9876ad5797a7e3ea8291c4cb
elvanrafif <elvan@transfez.com>
2023-08-05 15:41:15 +0700

    add new file

---
+++ abiaFOR.js
@@ -0,0 +1,15 @@
+// var jmAnkot = 10;
+// var angkotBeroperasi = 6;
+var jmAnkot = prompt('ada berapa jumlah angkot')
+var angkotBeroperasi = prompt('Ada berapa angkot yang beroperasi')
+
+for( var noAngkot = 1; noAngkot <= jmAnkot; noAngkot++ ) {
+
+  if( noAngkot <= 6 && noAngkot !== 5 ){
+    console.log('Angkot No. ' + noAngkot + ' Beroperasi dengan baik.');
+  } else if( noAngkot === 8 || noAngkot == 10 || noAngkot == 5) {
+    console.log('Angkot No. ' + noAngkot + ' Sedang lembur');
+  } else { 
+    console.log('Angkot No. ' + noAngkot + ' sedang tidak beroperasi.');
+  }
+}
